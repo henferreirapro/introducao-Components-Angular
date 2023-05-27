e49e676f520ce98c77fdd7459147795ccdfb1926
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2023-05-27 20:10:45 -0300

    Criando um component reutilizavel
    Criação  de um component reutilizável, add uma chamada para arquivo CSS e HTML externo ao invés de declarar tudo diretamente.

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu.component';
+import { Button } from './components/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
+    Button,
     
   ],
   imports: [
