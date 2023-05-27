a46435345b01abd9a7d861ec12167e3cbf7d85b1
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2023-05-27 19:28:05 -0300

    Criando um component simples

---
+++ my-app/src/app/components/menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'menu-component',
+  template: '<h2>Henrique</h2>',
+  styles: ['h2{ color: red }'],
+  
+})
+
+export class MenuComponent{
+  
+}
