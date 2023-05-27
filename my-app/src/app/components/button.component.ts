71c8ca94b1ae0b74798253a261dab4aaebf0d90c
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2023-05-27 20:37:50 -0300

    Add Interpolação de dados no component
    podemos add dados para usar dinamicamente em
    nossos components dentro da class Button no component.ts.
    Add os dados criando nome do atributo, tipo do dado(string ou button) e por fim o dado.
    add o dado dinamicamente no component.html usando {{}}, como fizemos no button.component.html

--- my-app/src/app/components/button.component.ts
+++ my-app/src/app/components/button.component.ts
@@ -8,5 +8,14 @@ import { Component } from '@angular/core';
 })
 
 export class Button {
-  
+  // add interpolação para deixar o texto dinamico.
+  buttonText: string = "ACESSAR";
+  buttonNumber: number = 28;
+  buttonTexts: string[] = ["VENDER", "PAGAR"];
+
 }
+
+// podemos add dados para usar dinamicamente em
+// nossos components dentro da class Button no component.ts.
+// Add os dados criando nome do atributo, tipo do dado(string ou button) e por fim o dado.
+// add o dado dinamicamente no component.html usando {{}}, como fizemos no button.component.html
