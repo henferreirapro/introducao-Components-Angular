29b20d4e065f8363d88de7c556605291571a198e
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2023-05-27 21:19:12 -0300

    Fazendo o componente pai conversar com o filho
    Add input no component filho, podemos add os dados pelo component pai e assim passar as propriedades diretamente no component dentro do app.component.html.
    
    Dessa forma  conseguimos fazer eles se conversarem e passarem os dados  atributos desejados  no component.
    
    Podemos declarar usando as duas formas recomendadas:
    [label]="variavel"
    label{{variavel}}
    
    Ambas  podem ser usadas na hora de declarar.

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  buttonLabel: string = "CARRINHO";
+  buttonAdd: string = "ADICIONAR";
+  buttonRemove: string = "REMOVER";
+  
 }
+
+// Add input no component filho(button.componet.ts), podemos add os dados pelo component pai e assim passar as propriedades diretamente no component dentro do app.component.html.
+
+// Dessa forma  conseguimos fazer eles se conversarem e passarem os dados  atributos desejados  no component.
+
+// Podemos declarar usando as duas formas recomendadas:
+// [label]="variavel"
+// label{{variavel}}
+
+// Ambas  podem ser usadas na hora de declarar.
--- my-app/src/app/components/button.component.ts
+++ my-app/src/app/components/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,  Input } from '@angular/core';
 
 @Component({
   selector: 'my-button',
@@ -8,14 +8,25 @@ import { Component } from '@angular/core';
 })
 
 export class Button {
+  @Input() label: string= "";
+
   // add interpolação para deixar o texto dinamico.
-  buttonText: string = "ACESSAR";
+  buttonText: string = "ACsESSAR";
   buttonNumber: number = 28;
   buttonTexts: string[] = ["VENDER", "PAGAR"];
 
 }
 
-// podemos add dados para usar dinamicamente em
-// nossos components dentro da class Button no component.ts.
-// Add os dados criando nome do atributo, tipo do dado(string ou button) e por fim o dado.
-// add o dado dinamicamente no component.html usando {{}}, como fizemos no button.component.html
+// INTERPOLAÇÃO
+  // podemos add dados para usar dinamicamente em
+  // nossos components dentro da class Button no component.ts.
+  // Add os dados criando nome do atributo, tipo do dado(string ou button) e por fim o dado.
+  // add o dado dinamicamente no component.html usando {{}}, como fizemos no button.component.html
+
+
+// ADD INPUT
+  // Add input no component filho(button.componet.ts), podemos add os dados pelo component pai e assim passar as propriedades diretamente no component dentro do app.component.html.
+  // Dessa forma  conseguimos fazer eles se conversarem e passarem os dados  atributos desejados  no component.
+  // Podemos declarar usando as duas formas recomendadas:
+  // [label]="variavel"
+  // label{{variavel}}
